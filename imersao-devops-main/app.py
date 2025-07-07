e8afb877682c856f89429ed7cc635e159cb015c9
Andressa Rocha <dessa.lstin@gmail.com>
2025-07-07 15:58:03 +0100

    DevOPs

---
+++ imersao-devops-main/app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from database import engine, Base
+from routers.alunos import alunos_router
+from routers.cursos import cursos_router
+from routers.matriculas import matriculas_router
+
+app = FastAPI(
+    title="API de Gestão Escolar", 
+    description="""
+        Esta API fornece endpoints para gerenciar alunos, cursos e turmas, em uma instituição de ensino.  
+        
+        Permite realizar diferentes operações em cada uma dessas entidades.
+    """, 
+    version="1.0.0",
+)
+
+@app.get("/")
+def home():
+    """Endpoint raiz para verificar se a API está no ar."""
+    return {"message": "API de Gestão Escolar no ar! Acesse /docs para a documentação."}
+
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(alunos_router, tags=["alunos"])
+app.include_router(cursos_router, tags=["cursos"])
+app.include_router(matriculas_router, tags=["matriculas"])
